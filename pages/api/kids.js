d6377f492ae1a30205d12c70a13845df55648fe0
eleftheria drakoulakou <drakoulakoue@kotsovolos.gr>
2023-10-30 01:08:06 +0200

    Remove vscode config & SQL Insert

--- pages/api/kids.js
+++ pages/api/kids.js
@@ -1,17 +1,16 @@
 import { sql } from "@vercel/postgres";
 
-const kids = [];
-
 export default async function handler(req, res) {
   try {
     if (req.method === "GET") {
-      const { rows } = await sql`SELECT * from Kids`;
-      res.status(200).json(rows); // response the kids
+      const { rows } = await sql`SELECT * from kids`;
+      res.status(200).json(rows);
     }
     if (req.method === "POST") {
-      const kid = JSON.parse(req.body); // JSON.parse transform string to object
-
-      res.status(200).json(kids); // REQUIRED response
+      const kid = JSON.parse(req.body);
+      await sql`INSERT INTO kids (firstname, lastname, age) VALUES (${kid.name},${kid.surname},${kid.age});`;
+      const { rows } = await sql`SELECT * from kids`;
+      res.status(200).json(rows);
     }
   } catch (err) {
     res.status(500).json({ message: err }); // catch errors
